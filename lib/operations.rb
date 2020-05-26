7f6264d4a7b993dfd33b88e29c5bfdea80b5de7f
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-26 17:19:00 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
 def unsafe?(speed)
-
+@speed = speed
+@speed < 40 || @speed > 60 ? true : false
 end
 
 
 
 def not_safe?(speed)
-	
+  @speed = speed
+  @speed < 40 || @speed > 60 ? true : false
 end
-	
-
-
